9d548c7eeb180fcd012bc7bee82387ae65acd94e
Emrullah Lüleci <emrullahluleci@gmail.com>
2018-10-13 13:55:26 +0100

    Show details on movie list row.

--- app/src/main/java/com/eluleci/tmdbshowcase/viewmodel/BindingAdapters.kt
+++ app/src/main/java/com/eluleci/tmdbshowcase/viewmodel/BindingAdapters.kt
@@ -2,8 +2,18 @@ package com.eluleci.tmdbshowcase.viewmodel
 
 import android.databinding.BindingAdapter
 import android.view.View
+import android.widget.ImageView
+import com.eluleci.tmdbshowcase.BuildConfig
+import com.squareup.picasso.Picasso
 
 @BindingAdapter("visibility")
 fun setVisibility(view: View, value: Boolean) {
     view.visibility = if (value) View.VISIBLE else View.GONE
+}
+
+@BindingAdapter("imageUrl")
+fun loadImage(view: ImageView, imageUrl: String?) {
+    if (imageUrl != null && !imageUrl.isEmpty()) {
+        Picasso.get().load(BuildConfig.IMAGE_URL + imageUrl).into(view)
+    }
 }
--- app/src/main/java/com/eluleci/tmdbshowcase/viewmodel/movielist/MovieListRowViewModel.kt
+++ app/src/main/java/com/eluleci/tmdbshowcase/viewmodel/movielist/MovieListRowViewModel.kt
@@ -4,8 +4,14 @@ import com.eluleci.tmdbshowcase.model.Movie
 
 class MovieListRowViewModel {
     lateinit var title: String
+    lateinit var posterPath: String
+    lateinit var overview: String
+    lateinit var voteAverage: String
 
     fun bind(movie: Movie) {
+        posterPath = movie.posterPath
+        overview = movie.overview
         title = movie.title
+        voteAverage = movie.voteAverage.toString()
     }
 }
